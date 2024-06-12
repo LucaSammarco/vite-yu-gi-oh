9c90e19048c1d869dd224d359963d21c2938f16b
Il tuo nome <la_tua_email@example.com>
2024-06-12 16:51:19 +0200

    Scaffolding

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+//import 
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+
+
+<style lang="scss">
+@use './styles/partial/variables' as *;
+@use './styles/partial/mixins' as *;
+@use './styles/general.scss';
+@use '../node_modules/bootstrap/scss/bootstrap.scss'
+
+
+</style>
